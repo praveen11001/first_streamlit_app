da9291421e9af1f7e01a46604e82a93f6f2cc136
praveen11001 <139953845+praveen11001@users.noreply.github.com>
2023-08-03 12:59:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Diner')
+streamlit.title('My Mom's new Healthy Diner')
+ 🥗  
+streamlit.header(' Breakdown Header')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞 Avocardo Toast')
 
-streamlit.header('Breakdown Header')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Ranged Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
